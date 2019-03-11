9dc9f7a336fd52e95958e43033b6f4f2da09b551
RAHUL KUMAR <rahulkrvnit@gmail.com>
2019-03-11 12:08:23 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-   ensure  => file,
-   content => 'This is a readme sitepp',
-   owner   => 'root',
-   }
-  file { '/root/README':
-    owner  => 'root',
-    }
+
 }
